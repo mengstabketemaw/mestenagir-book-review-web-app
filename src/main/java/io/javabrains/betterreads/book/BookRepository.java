129b17f407812680d5e4a014e82d58abb57d6a28
mengstabketemaw <mengstabrealk28@gmail.com>
2022-05-20 09:46:43 -0800

    a lot of things that i dont rememebr have happened

--- src/main/java/io/javabrains/betterreads/book/BookRepository.java
+++ src/main/java/io/javabrains/betterreads/book/BookRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CassandraRepository<Book, String> {
     @AllowFiltering
-    List<Book> findByName(String name);
+    List<Book> findByAddress(String address);
 }
